08ec1ecde3e89039f7d970f2a2b59ddb1303c3df
Thiago <throcha3@gmail.com>
2023-12-11 21:18:55 -0300

    feat: create navbar in main view and create register view

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,15 @@
 /** @type {import('tailwindcss').Config} */
 
 const colors = require('tailwindcss/colors')
+delete colors['lightBlue']
+
+delete colors['warmGray']
+
+delete colors['trueGray']
+
+delete colors['coolGray']
+
+delete colors['blueGray']
 
 module.exports = {
   content: ['./src/**/*.{vue,js}'],
